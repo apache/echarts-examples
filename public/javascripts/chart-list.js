75e5586e90b7959252bd804e80d8e8b899923083
pissang <bm2736892@gmail.com>
2017-03-24 10:51:55 +0800

    Update chart-list

--- public/javascripts/chart-list.js
+++ public/javascripts/chart-list.js
@@ -1,63 +1,63 @@
 var EXAMPLES = [
     {
         id: 'area-rainfall',
-        title: 'Rainfall and Water Flow',
+        title: '雨量流量关系图',
         type: 'line'
     },  {
         id: 'area-simple',
-        title: 'Area Chart',
+        title: '大数据量面积图',
         type: 'line'
     },  {
         id: 'area-stack',
-        title: 'Stacked Area',
+        title: '堆叠区域图',
         type: 'line'
     },  {
         id: 'bar-animation-delay',
-        title: 'Animation Delay on Bar Chart',
+        title: '柱状图动画延迟',
         type: 'bar'
     },  {
         id: 'bar-brush',
-        title: 'Brush on Bar Chart',
+        title: '柱状图框选',
         type: 'bar'
     },  {
         id: 'bar-gradient',
-        title: 'Gradient, Shadow, Click Zoom',
+        title: '特性示例：渐变色 阴影 点击缩放',
         type: 'bar'
     },  {
         id: 'bar-negative',
-        title: 'Negative Bar Chart',
+        title: '正负条形图',
         type: 'bar'
     },  {
         id: 'bar-negative2',
-        title: 'Negative and Positive',
+        title: '交错正负轴标签',
         type: 'bar'
     },  {
         id: 'bar-stack',
-        title: 'Stacked Bar',
+        title: '堆叠柱状图',
         type: 'bar'
     },  {
         id: 'bar-tick-align',
-        title: 'Align between Ticks and Labels',
+        title: '坐标轴刻度与标签对齐',
         type: 'bar'
     },  {
         id: 'bar-waterfall',
-        title: 'Minimum Cost of Living in Shenzhen (RMB)',
+        title: '深圳月最低生活费组成（单位:元）',
         type: 'bar'
     },  {
         id: 'bar-waterfall2',
-        title: 'Waterfall Chart',
+        title: '阶梯瀑布图',
         type: 'bar'
     },  {
         id: 'bar-y-category-stack',
-        title: 'Stacked Bar Chart',
+        title: '堆叠条形图',
         type: 'bar'
     },  {
         id: 'bar-y-category',
-        title: 'World Totle Population',
+        title: '世界人口总量 - 条形图',
         type: 'bar'
     },  {
         id: 'bar1',
-        title: 'Rainfall and Evaporation',
+        title: '某地区蒸发量和降水量',
         type: 'bar'
     },  {
         id: 'boxplot-light-velocity',
@@ -73,59 +73,91 @@ var EXAMPLES = [
         type: 'boxplot'
     },  {
         id: 'bubble-gradient',
-        title: 'Bubble Chart',
+        title: '气泡图',
         type: 'scatter'
+    },  {
+        id: 'calendar-charts',
+        title: 'Calendar Charts',
+        type: 'graph'
+    },  {
+        id: 'calendar-effectscatter',
+        title: '2016年某人每天的步数',
+        type: 'scatter'
+    },  {
+        id: 'calendar-graph',
+        title: 'Calendar Graph',
+        type: 'graph'
+    },  {
+        id: 'calendar-heatmap',
+        title: '2016年某人每天的步数',
+        type: 'heatmap'
+    },  {
+        id: 'calendar-horizontal',
+        title: 'Calendar Horizontal',
+        type: 'heatmap'
+    },  {
+        id: 'calendar-lunar',
+        title: '2017 三月',
+        type: 'scatter'
+    },  {
+        id: 'calendar-pie',
+        title: 'Calendar Pie',
+        type: 'scatter'
+    },  {
+        id: 'calendar-vertical',
+        title: 'Calendar Vertical',
+        type: 'heatmap'
     },  {
         id: 'candlestick-brush',
         title: 'Candlestick Brush',
         type: 'candlestick'
     },  {
         id: 'candlestick-sh-2015',
-        title: 'ShangHai Index, 2015',
+        title: '2015 年上证指数',
         type: 'candlestick'
     },  {
         id: 'candlestick-sh',
-        title: 'ShangHai Index',
+        title: '上证指数',
         type: 'candlestick'
     },  {
         id: 'candlestick-touch',
-        title: 'Axis Pointer Link and Touch',
-        type: 'candlestick'
+        title: '移动端 K线图',
+        type: 'bar'
     },  {
         id: 'confidence-band',
         title: 'Confidence Band',
         type: 'line'
     },  {
         id: 'dynamic-data',
-        title: 'Dynamic Data',
+        title: '动态数据',
         type: 'bar'
     },  {
         id: 'dynamic-data2',
-        title: 'Dynamic Data + Time Axis',
+        title: '动态数据 + 时间坐标轴',
         type: 'line'
     },  {
         id: 'effectScatter-bmap',
-        title: 'Air Quality - Baidu Map',
+        title: '全国主要城市空气质量 - 百度地图',
         type: 'scatter'
     },  {
         id: 'effectScatter-map',
-        title: 'Air Quality',
+        title: '全国主要城市空气质量',
         type: 'scatter'
     },  {
         id: 'funnel-align',
-        title: 'Funnel (align)',
+        title: '漏斗图(对比)',
         type: 'funnel'
     },  {
         id: 'funnel-customize',
-        title: 'Customized Funnel',
+        title: '漏斗图',
         type: 'funnel'
     },  {
         id: 'funnel-mutiple',
-        title: 'Multiple Funnels',
+        title: '漏斗图',
         type: 'funnel'
     },  {
         id: 'funnel',
-        title: 'Funnel Chart',
+        title: '漏斗图',
         type: 'funnel'
     },  {
         id: 'gauge-car-dark',
@@ -141,23 +173,27 @@ var EXAMPLES = [
         type: 'gauge'
     },  {
         id: 'geo-lines',
-        title: 'Migration',
+        title: '模拟迁徙',
         type: 'map'
+    },  {
+        id: 'geo-map-scatter',
+        title: 'Geo Map Scatter',
+        type: 'scatter'
     },  {
         id: 'graph-circular-layout',
         title: 'Les Miserables',
         type: 'graph'
     },  {
         id: 'graph-force',
-        title: 'Force Layout',
+        title: '力引导布局',
         type: 'graph'
     },  {
         id: 'graph-force2',
-        title: 'Force Layout',
+        title: '力引导布局',
         type: 'graph'
     },  {
         id: 'graph-grid',
-        title: 'Graph on Cartesian',
+        title: '笛卡尔坐标系上的 Graph',
         type: 'graph'
     },  {
         id: 'graph-life-expectancy',
@@ -169,7 +205,7 @@ var EXAMPLES = [
         type: 'graph'
     },  {
         id: 'graph-simple',
-        title: 'Simple Graph',
+        title: 'Graph 简单示例',
         type: 'graph'
     },  {
         id: 'graph-webkit-dep',
@@ -181,27 +217,27 @@ var EXAMPLES = [
         type: 'graph'
     },  {
         id: 'grid-multiple',
-        title: 'Rainfall and Water Flow',
+        title: '雨量流量关系图',
         type: 'line'
     },  {
         id: 'heatmap-bmap',
-        title: 'Heatmap on Baidu Map Extension',
+        title: '热力图与百度地图扩展',
         type: 'heatmap'
     },  {
         id: 'heatmap-cartesian',
-        title: 'Heatmap on Cartesian',
+        title: '笛卡尔坐标系上的热力图',
         type: 'heatmap'
     },  {
         id: 'heatmap-large-piecewise',
-        title: 'Heatmap - Discrete Mapping of Color',
+        title: '热力图 - 颜色的离散映射',
         type: 'heatmap'
     },  {
         id: 'heatmap-large',
-        title: 'Heatmap - 2w data',
+        title: '热力图 - 2w 数据',
         type: 'heatmap'
     },  {
         id: 'heatmap-map',
-        title: 'Air Qulity',
+        title: '全国主要城市空气质量',
         type: 'heatmap'
     },  {
         id: 'line-aqi',
@@ -216,12 +252,16 @@ var EXAMPLES = [
         title: 'Line Easing',
         type: 'line'
     },  {
+        id: 'line-graphic',
+        title: 'Line Graphic',
+        type: 'bar'
+    },  {
         id: 'line-log',
-        title: 'Log Axis',
+        title: '对数轴示例',
         type: 'line'
     },  {
         id: 'line-marker',
-        title: 'Temperature Change in the coming week',
+        title: '未来一周气温变化',
         type: 'line'
     },  {
         id: 'line-pen',
@@ -229,55 +269,51 @@ var EXAMPLES = [
         type: 'line'
     },  {
         id: 'line-polar',
-        title: 'Two Value-Axes in Polar',
+        title: '极坐标双数值轴',
         type: 'line'
     },  {
         id: 'line-polar2',
-        title: 'Two Value-Axes in Polar',
+        title: '极坐标双数值轴',
         type: 'line'
     },  {
         id: 'line-sections',
-        title: 'Distribution of Electricity',
+        title: '一天用电量分布',
         type: 'line'
     },  {
         id: 'line-stack',
-        title: 'Stacked Line Chart',
+        title: '折线图堆叠',
         type: 'line'
     },  {
         id: 'line-step',
         title: 'Step Line',
         type: 'line'
     },  {
+        id: 'line-tooltip-touch',
+        title: '触屏 tooltip 和 dataZoom 示例',
+        type: 'line'
+    },  {
         id: 'line-y-category',
         title: 'Line Y Category',
         type: 'line'
     },  {
-        id: 'multiple-x-axis',
-        title: 'Multiple X Axes',
-        type: 'line'
-    },  {
-        id: 'line-tooltip-touch',
-        title: 'Tooltip and DataZoom on Mobile',
-        type: 'line'
-    },  {
         id: 'lines-airline',
-        title: '65k+ Airline',
+        title: '65k+ 飞机航线',
         type: 'map'
     },  {
         id: 'lines-bmap-bus',
-        title: 'Bus Lines of Beijing - Baidu Map',
+        title: '北京公交路线 - 百度地图',
         type: 'map'
     },  {
         id: 'lines-bmap-effect',
-        title: 'Bus Lines of Beijing - Line Effect',
+        title: '北京公交路线 - 线特效',
         type: 'map'
     },  {
         id: 'lines-bmap',
-        title: 'A Hiking Trail in Hangzhou - Baidu Map',
+        title: '杭州热门步行路线 - 百度地图',
         type: 'map'
     },  {
         id: 'map-china-dataRange',
-        title: 'Sales of iphone',
+        title: 'iphone销量',
         type: 'map'
     },  {
         id: 'map-china',
@@ -285,15 +321,11 @@ var EXAMPLES = [
         type: 'map'
     },  {
         id: 'map-HK',
-        title: 'Population Density of HongKong (2011)',
+        title: '香港18区人口密度 （2011）',
         type: 'map'
     },  {
         id: 'map-locate',
         title: 'Map Locate',
-        type: 'map'
-    },  {
-        id: 'geo-map-scatter',
-        title: 'map and scatter share a geo',
         type: 'map'
     },  {
         id: 'map-parallel-prices',
@@ -301,7 +333,7 @@ var EXAMPLES = [
         type: 'scatter'
     },  {
         id: 'map-province',
-        title: 'Switch among 34 Provinces',
+        title: '34 省切换查看',
         type: 'map'
     },  {
         id: 'map-usa',
@@ -317,19 +349,23 @@ var EXAMPLES = [
         type: '0'
     },  {
         id: 'mix-line-bar',
-        title: 'Mixed Line and Bar',
+        title: '折柱混合',
         type: 'bar'
     },  {
         id: 'mix-timeline-finance',
-        title: 'Finance Indices 2002',
+        title: '2002全国宏观经济指标',
         type: 'bar'
     },  {
         id: 'mix-zoom-on-value',
         title: 'Mix Zoom On Value',
         type: 'bar'
     },  {
+        id: 'multiple-x-axis',
+        title: '多 X 轴示例',
+        type: 'line'
+    },  {
         id: 'multiple-y-axis',
-        title: 'Multiple Y Axes',
+        title: '多 Y 轴示例',
         type: 'bar'
     },  {
         id: 'parallel-aqi',
@@ -340,48 +376,76 @@ var EXAMPLES = [
         title: 'Parallel Nutrients',
         type: 'parallel'
     },  {
+        id: 'pictorialBar-body-fill',
+        title: 'PictorialBar Body Fill',
+        type: 'pictorialBar'
+    },  {
+        id: 'pictorialBar-dotted',
+        title: 'PictorialBar Dotted',
+        type: 'line'
+    },  {
+        id: 'pictorialBar-forest',
+        title: 'PictorialBar Forest',
+        type: 'pictorialBar'
+    },  {
+        id: 'pictorialBar-hill',
+        title: 'PictorialBar Hill',
+        type: 'pictorialBar'
+    },  {
+        id: 'pictorialBar-spirit',
+        title: 'PictorialBar Spirit',
+        type: 'pictorialBar'
+    },  {
+        id: 'pictorialBar-vehicle',
+        title: 'Vehicles in X City',
+        type: 'pictorialBar'
+    },  {
+        id: 'pictorialBar-velocity',
+        title: 'PictorialBar Velocity',
+        type: 'pictorialBar'
+    },  {
         id: 'pie-custom',
         title: 'Customized Pie',
         type: 'pie'
     },  {
         id: 'pie-doughnut',
-        title: 'Doughnut Chart',
+        title: '环形图',
         type: 'pie'
     },  {
         id: 'pie-nest',
-        title: 'Nested Pies',
+        title: '嵌套环形图',
         type: 'pie'
     },  {
         id: 'pie-pattern',
-        title: 'Texture on Pie Chart',
+        title: '饼图纹理',
         type: 'pie'
     },  {
         id: 'pie-roseType',
-        title: 'Nightingale\'s Rose Diagram',
+        title: '南丁格尔玫瑰图',
         type: 'pie'
     },  {
         id: 'pie-simple',
-        title: 'Referer of a website',
+        title: '某站点用户访问来源',
         type: 'pie'
     },  {
         id: 'radar-aqi',
-        title: 'AQI - Radar Chart',
+        title: 'AQI - 雷达图',
         type: 'radar'
     },  {
         id: 'radar-custom',
-        title: 'Customized Radar Chart',
+        title: '自定义雷达图',
         type: 'radar'
     },  {
         id: 'radar-multiple',
-        title: 'Multiple Radar',
+        title: '多雷达图',
         type: 'radar'
     },  {
         id: 'radar',
-        title: 'Basic Radar Chart',
+        title: '基础雷达图',
         type: 'radar'
     },  {
         id: 'radar2',
-        title: 'Propotion of Browsers',
+        title: '浏览器占比变化',
         type: 'radar'
     },  {
         id: 'sankey-energy',
@@ -401,11 +465,11 @@ var EXAMPLES = [
         type: 'scatter'
     },  {
         id: 'scatter-large',
-        title: 'Large Scatter',
+        title: '大规模散点图',
         type: 'scatter'
     },  {
         id: 'scatter-life-expectancy-timeline',
-        title: 'Life Expectancy and GDP',
+        title: '各国人均寿命与GDP关系演变',
         type: 'scatter'
     },  {
         id: 'scatter-map-brush',
@@ -413,12 +477,16 @@ var EXAMPLES = [
         type: 'scatter'
     },  {
         id: 'scatter-map',
-        title: 'Air Quality',
+        title: '全国主要城市空气质量',
         type: 'scatter'
     },  {
         id: 'scatter-matrix',
         title: 'Scatter Matrix',
         type: 'parallel'
+    },  {
+        id: 'scatter-nutrients-matrix',
+        title: 'Scatter Nutrients Matrix',
+        type: 'scatter'
     },  {
         id: 'scatter-nutrients',
         title: 'Scatter Nutrients',
@@ -437,31 +505,35 @@ var EXAMPLES = [
         type: 'scatter'
     },  {
         id: 'scatter-single-axis',
-        title: 'Scatter on Single Axis',
+        title: '单轴散点图',
         type: 'scatter'
     },  {
         id: 'scatter-weibo',
-        title: 'Sign in of weibo',
+        title: '微博签到数据点亮中国',
         type: 'scatter'
     },  {
         id: 'scatter-weight',
-        title: 'Distribution of Height and Weight',
+        title: '男性女性身高体重分布',
         type: 'scatter'
     },  {
         id: 'scatter-world-population',
         title: 'World Population (2011)',
         type: 'scatter'
     },  {
-        id: 'scatter-nutrients-matrix',
-        title: 'Scatter Nutrients Matrix',
-        type: 'scatter'
+        id: 'themeRiver-basic',
+        title: 'ThemeRiver Basic',
+        type: 'themeRiver'
+    },  {
+        id: 'themeRiver-lastfm',
+        title: 'ThemeRiver Lastfm',
+        type: 'themeRiver'
     },  {
         id: 'treemap-disk',
         title: 'Disk Usage',
         type: 'treemap'
     },  {
         id: 'treemap-drill-down',
-        title: 'ECharts Option Query',
+        title: 'ECharts 配置项查询分布',
         type: 'treemap'
     },  {
         id: 'treemap-obama',
@@ -473,85 +545,9 @@ var EXAMPLES = [
         type: 'treemap'
     },  {
         id: 'watermark',
-        title: 'Watermark - ECharts Download',
+        title: '水印 - ECharts 下载统计',
         type: 'bar'
-    }, {
-        id: 'pictorialBar-hill',
-        title: 'Wish List and Mountain Height',
-        type: 'pictorialBar'
-    }, {
-        id: 'pictorialBar-velocity',
-        title: 'Velocity of Christmas Reindeers',
-        type: 'pictorialBar'
-    }, {
-        id: 'pictorialBar-vehicle',
-        title: 'Vehicles',
-        type: 'pictorialBar'
-    }, {
-        id: 'pictorialBar-spirit',
-        title: 'Spirits',
-        type: 'pictorialBar'
-    }, {
-        id: 'pictorialBar-body-fill',
-        title: 'Water Content',
-        type: 'pictorialBar'
-    }, {
-        id: 'pictorialBar-dotted',
-        title: 'Dotted bar',
-        type: 'pictorialBar'
-    }, {
-        id: 'pictorialBar-forest',
-        title: 'Expansion of forest',
-        type: 'pictorialBar'
-    }, {
-        id: 'themeRiver-basic',
-        title: 'ThemeRiver',
-        type: 'themeRiver'
-    }, {
-        id: 'themeRiver-lastfm',
-        title: 'ThemeRiver Lastfm',
-        type: 'themeRiver'
-    }, {
-        id: 'calendar-graph',
-        title: 'Calendar Graph',
-        type: 'graph'
-    }, {
-        id: 'calendar-pie',
-        title: 'Calendar Pie',
-        type: 'calendar'
-    }, {
-        id: 'calendar-effectscatter',
-        title: 'Calendar Effectscatter',
-        type: 'scatter'
-    }, {
-        id: 'calendar-vertical',
-        title: 'Calendar Heatmap Vertical',
-        type: 'heatmap'
-    }, {
-        id: 'calendar-horizontal',
-        title: 'Calendar Heatmap Horizontal',
-        type: 'heatmap'
-    }, {
-        id: 'calendar-effectscatter',
-        title: 'Calendar Effectscatter',
-        type: 'calendar'
-    }, {
-        id: 'calendar-vertical',
-        title: 'Calendar Heatmap Vertical',
-        type: 'calendar'
-    }, {
-        id: 'calendar-horizontal',
-        title: 'Calendar Heatmap Horizontal',
-        type: 'calendar'
-    }, {
-        id: 'calendar-charts',
-        title: 'Calendar Charts',
-        type: 'calendar'
-    }, {
-        id: 'calendar-lunar',
-        title: 'Calendar Lunar',
-        type: 'calendar'
-    }
+    } 
 ];
 
 var CHART_TYPES = {
@@ -571,8 +567,7 @@ var CHART_TYPES = {
     funnel: 'Funnel',
     gauge: 'Gauge',
     pictorialBar: 'PictorialBar',
-    themeRiver: 'ThemeRiver',
-    calendar: 'Calendar'
+    themeRiver: 'ThemeRiver'
 };
 
 var charts = [];
