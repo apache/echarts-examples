fa243898b9957d64790f1dd122607599d38c4a41
pissang <bm2736892@gmail.com>
2018-01-13 15:37:21 +0800

    Add theme popover

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -67,6 +67,11 @@ var gb = {
     debounceTime: 500,
 };
 
+var COLORS = {
+    default: ['#c23531','#2f4554', '#61a0a8', '#d48265', '#91c7ae','#749f83',  '#ca8622', '#bda29a','#6e7074', '#546570', '#c4ccd3'],
+    light: ['#37A2DA', '#32C5E9', '#67E0E3', '#9FE6B8', '#FFDB5C','#ff9f7f', '#fb7293', '#E062AE', '#E690D1', '#e7bcf3', '#9d96f5', '#8378EA', '#96BFFF'],
+    dark: ['#dd6b66','#759aa0','#e69d87','#8dc1a9','#ea7e53','#eedd78','#73a373','#73b9bc','#7289ab', '#91ca8c','#f49f42']
+};
 
 $('#theme .' + (configs.theme || 'default')).addClass('selected');
 
@@ -81,6 +86,20 @@ $('#theme a').click(function () {
         gl: configs.gl || null,
         theme: theme || null
     });
+})
+// Add popover
+$('#theme a').popover({
+    html: true,
+    content: function () {
+        var theme = $(this).attr('class').replace('selected', '').trim();
+        return '<div class="theme-palette ' + theme + '">'
+            + COLORS[theme].map(function (color) {
+                return '<span style="background-color:' + color + '"></span>'
+            }).join('')
+            + '</div>';
+    },
+    placement: 'top',
+    trigger: 'hover'
 });
 
 
