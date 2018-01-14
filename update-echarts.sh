ce3ee8f8c79bf1cfa3f1db96ca5549cc909d83da
pissang <bm2736892@gmail.com>
2018-01-14 12:34:20 +0800

    tweak

--- update-echarts.sh
+++ update-echarts.sh
@@ -7,8 +7,6 @@ node ${ECHARTS_DIR}/build/build.js --min
 
 cp ${ECHARTS_DIR}/dist/echarts.min.js ./public/vendors/echarts/echarts.min.js
 cp ${ECHARTS_DIR}/dist/echarts.js ./public/vendors/echarts/echarts.js
-rm -r ./public/vendors/echarts/extension
 cp -r ${ECHARTS_DIR}/dist/extension ./public/vendors/echarts/
 rm -r ./public/vendors/echarts/map
 cp -r ${ECHARTS_DIR}/map ./public/vendors/echarts/
-
