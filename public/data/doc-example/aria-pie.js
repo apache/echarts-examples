7e1eca87106cc1e29c4f344c860116b0f1a6443d
Ovilia <zwl.sjtu@gmail.com>
2018-01-15 15:55:56 +0800

    add aria example

---
+++ public/data/doc-example/aria-pie.js
@@ -0,0 +1,22 @@
+option = {
+    aria: {
+        show: true
+    },
+    title: {
+        text: '某站点用户访问来源',
+        x: 'center'
+    },
+    series: [
+        {
+            name: '访问来源',
+            type: 'pie',
+            data: [
+                { value: 335, name: '直接访问' },
+                { value: 310, name: '邮件营销' },
+                { value: 234, name: '联盟广告' },
+                { value: 135, name: '视频广告' },
+                { value: 1548, name: '搜索引擎' }
+            ]
+        }
+    ]
+};
