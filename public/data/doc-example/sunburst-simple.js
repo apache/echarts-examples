e453f9cdba148047ac8bb194d284fa51f52821e3
Ovilia <zwl.sjtu@gmail.com>
2018-01-17 14:06:40 +0800

    add example

---
+++ public/data/doc-example/sunburst-simple.js
@@ -0,0 +1,28 @@
+var option = {
+    series: {
+        type: 'sunburst',
+        data: [{
+            name: 'A',
+            value: 10,
+            children: [{
+                value: 3,
+                name: 'Aa'
+            }, {
+                value: 5,
+                name: 'Ab'
+            }]
+        }, {
+            name: 'B',
+            children: [{
+                name: 'Ba',
+                value: 4
+            }, {
+                name: 'Bb',
+                value: 2
+            }]
+        }, {
+            name: 'C',
+            value: 3
+        }]
+    }
+};
