16cc28ff581398e3d18a9f8522c84c472097b265
sushuang <sushuang0322@gmail.com>
2018-09-12 00:56:39 +0800

    Add sankey vertical example.

---
+++ public/data/meta/sankey-vertical.md
@@ -0,0 +1,5 @@
+---
+title: Sankey Diagram Vertical
+category: sankey
+titleCN: Sankey Diagram Vertical
+---
---
+++ public/data/sankey-vertical.js
@@ -0,0 +1,44 @@
+option = {
+    color: [
+        '#67001f', '#b2182b', '#d6604d', '#f4a582', '#fddbc7', '#d1e5f0', '#92c5de', '#4393c3', '#2166ac', '#053061'
+    ],
+    tooltip: {
+        trigger: 'item',
+        triggerOn: 'mousemove'
+    },
+    animation: false,
+    series: [
+        {
+            type: 'sankey',
+            layout:'none',
+            bottom: '10%',
+            focusNodeAdjacency: 'allEdges',
+            data: [
+                {name: 'a'},
+                {name: 'b'},
+                {name: 'a1'},
+                {name: 'b1'},
+                {name: 'c'},
+                {name: 'e'}
+            ],
+            links: [
+                {source: 'a', target: 'a1', value: 5},
+                {source: 'e', target: 'b', value: 3},
+                {source: 'a', target: 'b1', value: 3},
+                {source: 'b1', target: 'a1', value: 1},
+                {source: 'b1', target: 'c', value: 2},
+                {source: 'b', target: 'c', value: 1}
+            ],
+            orient: 'vertical',
+            label: {
+                position: 'top'
+            },
+            lineStyle: {
+                normal: {
+                    color: 'source',
+                    curveness: 0.5
+                }
+            }
+        }
+    ]
+}
