d56786eccc766895cbf8574427a53e586d4fdf21
deqingli <annong035@gmail.com>
2018-10-22 14:23:23 +0800

    feat(sankey): add node align right example

---
+++ public/data/meta/sankey-nodeAlign-right.md
@@ -0,0 +1,5 @@
+---
+title: Sankey Node Align Right
+category: sankey
+titleCN: Sankey Node Align Right
+---
---
+++ public/data/sankey-nodeAlign-right.js
@@ -0,0 +1,32 @@
+myChart.showLoading();
+$.get('data/asset/data/energy.json', function (data) {
+    myChart.hideLoading();
+
+    myChart.setOption(option = {
+        title: {
+            text: 'Node Align Left'
+        },
+        tooltip: {
+            trigger: 'item',
+            triggerOn: 'mousemove'
+        },
+        animation: false,
+        series: [
+            {
+                type: 'sankey',
+                layout:'none',
+                focusNodeAdjacency: true,
+                nodeAlign: 'right',
+                data: data.nodes,
+                links: data.links,
+                lineStyle: {
+                    normal: {
+                        color: 'source',
+                        curveness: 0.5
+                    }
+                }
+            }
+        ]
+    });
+});
+
