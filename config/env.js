1dfa70a857aec30d58e1a3f66e95e5b7f319d6ea
Ovilia <zwl.sjtu@gmail.com>
2019-05-30 16:09:09 +0800

    fix domain name

--- config/env.js
+++ config/env.js
@@ -2,7 +2,7 @@ module.exports = {
 
     version: '4.2.1',
 
-    host: 'http://apache.echarts.org/examples',
+    host: 'http://echarts.apache.org/examples',
     debugHost: 'http://localhost/incubator-echarts-website/examples',
     mainSitePath: 'https://echarts.apache.org',
     debugMainSitePath: 'http://localhost/incubator-echarts-website',
