fda20de4a7df98dff78cc7b1c72559ad303d0708
Ovilia <zwl.sjtu@gmail.com>
2019-12-05 05:22:37 +0800

    add examples for apache/incubator-echarts#11715

---
+++ public/data/doc-example/pie-label-bleedMargin.js
@@ -0,0 +1,69 @@
+var data = [{
+    name: 'Apple Numbers',
+    value: 70
+}, {
+    name: 'Strawberry Numbers',
+    value: 68
+}, {
+    name: 'Banana Numbers',
+    value: 48
+}, {
+    name: 'Orange Numbers',
+    value: 40
+}, {
+    name: 'Pear Numbers',
+    value: 32
+}, {
+    name: 'Pineapple Numbers',
+    value: 27
+}, {
+    name: 'Grape Numbers',
+    value: 18
+}];
+
+option = {
+    title: [{
+        text: 'Pie label margin'
+    }, {
+        subtext: 'bleedMargin: 50',
+        left: '25%',
+        top: '75%',
+        textAlign: 'center'
+    }, {
+        subtext: 'bleedMargin: 10',
+        left: '75%',
+        top: '75%',
+        textAlign: 'center'
+    }],
+    series: [{
+        type: 'pie',
+        radius: '25%',
+        center: ['50%', '50%'],
+        data: data,
+        animation: false,
+        label: {
+            position: 'outer',
+            alignTo: 'labelLine',
+            bleedMargin: 50
+        },
+        left: 0,
+        right: '50%',
+        top: 0,
+        bottom: 0
+    }, {
+        type: 'pie',
+        radius: '25%',
+        center: ['50%', '50%'],
+        data: data,
+        animation: false,
+        label: {
+            position: 'outer',
+            alignTo: 'labelLine',
+            bleedMargin: 10
+        },
+        left: '50%',
+        right: 0,
+        top: 0,
+        bottom: 0
+    }]
+};
---
+++ public/data/pie-alignTo.js
@@ -0,0 +1,89 @@
+var data = [{
+    name: 'Apples',
+    value: 70
+}, {
+    name: 'Strawberries',
+    value: 68
+}, {
+    name: 'Bananas',
+    value: 48
+}, {
+    name: 'Oranges',
+    value: 40
+}, {
+    name: 'Pears',
+    value: 32
+}, {
+    name: 'Pineapples',
+    value: 27
+}, {
+    name: 'Grapes',
+    value: 18
+}];
+
+option = {
+    title: [{
+        text: 'Pie label alignTo'
+    }, {
+        subtext: 'alignTo: "none" (default)',
+        left: '16.67%',
+        top: '75%',
+        textAlign: 'center'
+    }, {
+        subtext: 'alignTo: "labelLine"',
+        left: '50%',
+        top: '75%',
+        textAlign: 'center'
+    }, {
+        subtext: 'alignTo: "edge"',
+        left: '83.33%',
+        top: '75%',
+        textAlign: 'center'
+    }],
+    series: [{
+        type: 'pie',
+        radius: '25%',
+        center: ['50%', '50%'],
+        data: data,
+        animation: false,
+        label: {
+            position: 'outer',
+            alignTo: 'none',
+            bleedMargin: 5
+        },
+        left: 0,
+        right: '66.6667%',
+        top: 0,
+        bottom: 0
+    }, {
+        type: 'pie',
+        radius: '25%',
+        center: ['50%', '50%'],
+        data: data,
+        animation: false,
+        label: {
+            position: 'outer',
+            alignTo: 'labelLine',
+            bleedMargin: 5
+        },
+        left: '33.3333%',
+        right: '33.3333%',
+        top: 0,
+        bottom: 0
+    }, {
+        type: 'pie',
+        radius: '25%',
+        center: ['50%', '50%'],
+        data: data,
+        animation: false,
+        label: {
+            position: 'outer',
+            alignTo: 'edge',
+            margin: 20
+        },
+        left: '66.6667%',
+        right: 0,
+        top: 0,
+        bottom: 0
+    }]
+};
