bfd64454689df6e8be77726a25757078d7015640
pissang <bm2736892@gmail.com>
2019-12-25 13:51:53 +0800

    Add two line case for minorTick and minorSplitLine

---
+++ public/data/line-function.js
@@ -0,0 +1,81 @@
+function func(x) {
+    x /= 10;
+    return Math.sin(x) * Math.cos(x * 2 + 1) * Math.sin(x * 3 + 2) * 50;
+}
+
+
+function generateData() {
+    let data = [];
+    for (let i = -100; i <= 100; i += 0.1) {
+        data.push([i, func(i)]);
+    }
+    return data;
+}
+
+option = {
+    animation: false,
+    grid: {
+        containLabel: true
+    },
+    xAxis: {
+        name: 'x',
+        min: -100,
+        max: 100,
+        minorTick: {
+            show: true
+        },
+        splitLine: {
+            lineStyle: {
+                color: '#999'
+            }
+        },
+        minorSplitLine: {
+            show: true,
+            lineStyle: {
+                color: '#ddd'
+            }
+        }
+    },
+    yAxis: {
+        name: 'y',
+        min: -100,
+        max: 100,
+        minorTick: {
+            show: true
+        },
+        splitLine: {
+            lineStyle: {
+                color: '#999'
+            }
+        },
+        minorSplitLine: {
+            show: true,
+            lineStyle: {
+                color: '#ddd'
+            }
+        }
+    },
+    dataZoom: [{
+        show: true,
+        type: 'inside',
+        filterMode: 'none',
+        xAxisIndex: [0],
+        startValue: -10,
+        endValue: 10
+    }, {
+        show: true,
+        type: 'inside',
+        filterMode: 'none',
+        yAxisIndex: [0],
+        startValue: -10,
+        endValue: 10
+    }],
+    series: [
+        {
+            type: 'line',
+            showSymbol: false,
+            clip: true,
+            data: generateData()
+        }
+    ]
+};
