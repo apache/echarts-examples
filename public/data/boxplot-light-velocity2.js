b31b4880e5621d63d2579e580529e4edb20f29fd
100pah <sushuang0322@gmail.com>
2020-08-07 02:10:52 +0800

    fix: boxplot examples for 5.0

--- public/data/boxplot-light-velocity2.js
+++ public/data/boxplot-light-velocity2.js
@@ -1,13 +1,3 @@
-var data = echarts.dataTool.prepareBoxplotData([
-    [850, 740, 900, 1070, 930, 850, 950, 980, 980, 880, 1000, 980, 930, 650, 760, 810, 1000, 1000, 960, 960],
-    [960, 940, 960, 940, 880, 800, 850, 880, 900, 840, 830, 790, 810, 880, 880, 830, 800, 790, 760, 800],
-    [880, 880, 880, 860, 720, 720, 620, 860, 970, 950, 880, 910, 850, 870, 840, 840, 850, 840, 840, 840],
-    [890, 810, 810, 820, 800, 770, 760, 740, 750, 760, 910, 920, 890, 860, 880, 720, 840, 850, 850, 780],
-    [890, 840, 780, 810, 760, 810, 790, 810, 820, 850, 870, 870, 810, 740, 810, 940, 950, 800, 810, 870]
-], {
-    layout: 'vertical'
-});
-
 option = {
     title: [
         {
@@ -25,6 +15,27 @@ option = {
             top: '90%'
         }
     ],
+    dataset: [{
+        source: [
+            [850, 740, 900, 1070, 930, 850, 950, 980, 980, 880, 1000, 980, 930, 650, 760, 810, 1000, 1000, 960, 960],
+            [960, 940, 960, 940, 880, 800, 850, 880, 900, 840, 830, 790, 810, 880, 880, 830, 800, 790, 760, 800],
+            [880, 880, 880, 860, 720, 720, 620, 860, 970, 950, 880, 910, 850, 870, 840, 840, 850, 840, 840, 840],
+            [890, 810, 810, 820, 800, 770, 760, 740, 750, 760, 910, 920, 890, 860, 880, 720, 840, 850, 850, 780],
+            [890, 840, 780, 810, 760, 810, 790, 810, 820, 850, 870, 870, 810, 740, 810, 940, 950, 800, 810, 870]
+        ]
+    }, {
+        transform: {
+            type: 'boxplot',
+            config: {
+                itemNameFormatter: function (params) {
+                    return 'expr ' + params.value;
+                }
+            }
+        }
+    }, {
+        fromDatasetIndex: 1,
+        fromTransformResult: 1
+    }],
     tooltip: {
         trigger: 'item',
         axisPointer: {
@@ -38,14 +49,10 @@ option = {
     },
     yAxis: {
         type: 'category',
-        data: data.axisData,
         boundaryGap: true,
         nameGap: 30,
         splitArea: {
             show: false
-        },
-        axisLabel: {
-            formatter: 'expr {value}'
         },
         splitLine: {
             show: false
@@ -62,24 +69,13 @@ option = {
         {
             name: 'boxplot',
             type: 'boxplot',
-            data: data.boxData,
-            tooltip: {
-                formatter: function (param) {
-                    return [
-                        'Experiment ' + param.name + ': ',
-                        'upper: ' + param.data[5],
-                        'Q3: ' + param.data[4],
-                        'median: ' + param.data[3],
-                        'Q1: ' + param.data[2],
-                        'lower: ' + param.data[1]
-                    ].join('<br/>')
-                }
-            }
+            datasetIndex: 1
         },
         {
             name: 'outlier',
             type: 'scatter',
-            data: data.outliers
+            encode: { x: 1, y: 0 },
+            datasetIndex: 2
         }
     ]
 };
--- public/data/boxplot-multi.js
+++ public/data/boxplot-multi.js
@@ -1,17 +1,19 @@
 
 // Generate data.
-var data = [];
-for (var seriesIndex = 0; seriesIndex < 5; seriesIndex++) {
-    var seriesData = [];
+function makeData() {
+    var data = [];
     for (var i = 0; i < 18; i++) {
         var cate = [];
         for (var j = 0; j < 100; j++) {
             cate.push(Math.random() * 200);
         }
-        seriesData.push(cate);
+        data.push(cate);
     }
-    data.push(echarts.dataTool.prepareBoxplotData(seriesData));
+    return data;
 }
+var data0 = makeData();
+var data1 = makeData();
+var data2 = makeData();
 
 
 option = {
@@ -19,9 +21,24 @@ option = {
         text: 'Multiple Categories',
         left: 'center',
     },
+    dataset: [{
+        source: data0
+    }, {
+        source: data1
+    }, {
+        source: data2
+    }, {
+        fromDatasetIndex: 0,
+        transform: { type: 'boxplot' }
+    }, {
+        fromDatasetIndex: 1,
+        transform: { type: 'boxplot' }
+    }, {
+        fromDatasetIndex: 2,
+        transform: { type: 'boxplot' }
+    }],
     legend: {
-        top: '10%',
-        data: ['category0', 'category1', 'category2', 'category3']
+        top: '10%'
     },
     tooltip: {
         trigger: 'item',
@@ -37,14 +54,10 @@ option = {
     },
     xAxis: {
         type: 'category',
-        data: data[0].axisData,
         boundaryGap: true,
         nameGap: 30,
         splitArea: {
             show: true
-        },
-        axisLabel: {
-            formatter: 'expr {value}'
         },
         splitLine: {
             show: false
@@ -59,50 +72,29 @@ option = {
             show: false
         }
     },
-    dataZoom: [
-        {
-            type: 'inside',
-            start: 0,
-            end: 20
-        },
-        {
-            show: true,
-            type: 'slider',
-            top: '90%',
-            xAxisIndex: [0],
-            start: 0,
-            end: 20
-        }
-    ],
-    series: [
-        {
-            name: 'category0',
-            type: 'boxplot',
-            data: data[0].boxData,
-            tooltip: {formatter: formatter}
-        },
-        {
-            name: 'category1',
-            type: 'boxplot',
-            data: data[1].boxData,
-            tooltip: {formatter: formatter}
-        },
-        {
-            name: 'category2',
-            type: 'boxplot',
-            data: data[2].boxData,
-            tooltip: {formatter: formatter}
-        }
-    ]
+    dataZoom: [{
+        type: 'inside',
+        start: 0,
+        end: 20
+    }, {
+        show: true,
+        type: 'slider',
+        top: '90%',
+        xAxisIndex: [0],
+        start: 0,
+        end: 20
+    }],
+    series: [{
+        name: 'category0',
+        type: 'boxplot',
+        datasetIndex: 3
+    }, {
+        name: 'category1',
+        type: 'boxplot',
+        datasetIndex: 4
+    }, {
+        name: 'category2',
+        type: 'boxplot',
+        datasetIndex: 5
+    }]
 };
-
-function formatter(param) {
-    return [
-        'Experiment ' + param.name + ': ',
-        'upper: ' + param.data[0],
-        'Q1: ' + param.data[1],
-        'median: ' + param.data[2],
-        'Q3: ' + param.data[3],
-        'lower: ' + param.data[4]
-    ].join('<br/>');
-}
