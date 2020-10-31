8bd342b97e6e87523e6eeb8d9006f875c01eb2d3
100pah <sushuang0322@gmail.com>
2020-11-01 00:27:49 +0800

    change cdn pay root to jsdelivr

--- config/env.asf.js
+++ config/env.asf.js
@@ -4,11 +4,13 @@ const config = require('./common');
 Object.assign(config, {
     host: 'https://echarts.apache.org/examples',
     cdnPayRootMap: {
-        zh: 'https://echarts-www.cdn.bcebos.com/examples',
+        // zh: 'https://echarts-www.cdn.bcebos.com/examples',
+        zh: 'https://cdn.jsdelivr.net/gh/apache/incubator-echarts-website@asf-site/examples',
         en: 'https://echarts.apache.org/examples'
     },
     mainSiteCDNPayRootMap: {
-        zh: 'https://echarts-www.cdn.bcebos.com',
+        // zh: 'https://echarts-www.cdn.bcebos.com',
+        zh: 'https://cdn.jsdelivr.net/gh/apache/incubator-echarts-website@asf-site',
         en: 'https://echarts.apache.org'
     },
     mainSiteHost: 'https://echarts.apache.org',
