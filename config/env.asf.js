b36ff7a860f337c0ed39b648472f9a81742568bc
pissang <bm2736892@gmail.com>
2021-01-06 20:32:29 +0800

    migrate v5.x to the entry

--- config/env.asf.js
+++ config/env.asf.js
@@ -2,7 +2,7 @@ const path = require('path');
 const config = require('./common');
 
 Object.assign(config, {
-    releaseDestDir: path.resolve(__dirname, '../../incubator-echarts-website/next/examples')
+    releaseDestDir: path.resolve(__dirname, '../../incubator-echarts-website/examples')
 });
 
 module.exports = config;
