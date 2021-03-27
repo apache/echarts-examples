148ed157e76f6c51e5ffb1fc1a4a13ce67661f33
plainheart <yhen@all-my-life.cn>
2021-03-27 09:25:39 +0800

    fix(radar): fix a typo, resolves #33.

--- public/data/radar.js
+++ public/data/radar.js
@@ -26,7 +26,7 @@ option = {
         indicator: [
             { name: '销售（sales）', max: 6500},
             { name: '管理（Administration）', max: 16000},
-            { name: '信息技术（Information Techology）', max: 30000},
+            { name: '信息技术（Information Technology）', max: 30000},
             { name: '客服（Customer Support）', max: 38000},
             { name: '研发（Development）', max: 52000},
             { name: '市场（Marketing）', max: 25000}
