28255a8fc7334c8bb06623b320b9acc28d50ad7c
plainheart <yhen@all-my-life.cn>
2022-11-11 18:51:02 +0800

    fix: remove deprecated `normal` level from some examples

--- public/examples/ts/doc-example/text-block-fragment.js
+++ public/examples/ts/doc-example/text-block-fragment.js
@@ -1,67 +1,64 @@
-
 option = {
-    series: [
-        {
-            type: 'scatter',
-            data: [[0,0]],
-            symbolSize: 1,
-            label: {
-                normal: {
-                    show: true,
-                    formatter: [
-                        'The whole box is a {term|Text Block}, with',
-                        'red border and grey background.',
-                        '{fragment1|A Text Fragment} {fragment2|Another Text Fragment}',
-                        'Text fragments can be customized.'
-                    ].join('\n'),
-                    backgroundColor: '#eee',
-                    // borderColor: '#333',
-                    borderColor: 'rgb(199,86,83)',
-                    borderWidth: 2,
-                    borderRadius: 5,
-                    padding: 10,
-                    color: '#000',
-                    fontSize: 14,
-                    shadowBlur: 3,
-                    shadowColor: '#888',
-                    shadowOffsetX: 0,
-                    shadowOffsetY: 3,
-                    lineHeight: 30,
-                    rich: {
-                        term: {
-                            fontSize: 18,
-                            color: 'rgb(199,86,83)'
-                        },
-                        fragment1: {
-                            backgroundColor: '#000',
-                            color: 'yellow',
-                            padding: 5
-                        },
-                        fragment2: {
-                            backgroundColor: '#339911',
-                            color: '#fff',
-                            borderRadius: 15,
-                            padding: 5
-                        }
-                    }
-                }
-            }
+  series: [
+    {
+      type: 'scatter',
+      data: [[0, 0]],
+      symbolSize: 1,
+      label: {
+        show: true,
+        formatter: [
+          'The whole box is a {term|Text Block}, with',
+          'red border and grey background.',
+          '{fragment1|A Text Fragment} {fragment2|Another Text Fragment}',
+          'Text fragments can be customized.'
+        ].join('\n'),
+        backgroundColor: '#eee',
+        // borderColor: '#333',
+        borderColor: 'rgb(199,86,83)',
+        borderWidth: 2,
+        borderRadius: 5,
+        padding: 10,
+        color: '#000',
+        fontSize: 14,
+        shadowBlur: 3,
+        shadowColor: '#888',
+        shadowOffsetX: 0,
+        shadowOffsetY: 3,
+        lineHeight: 30,
+        rich: {
+          term: {
+            fontSize: 18,
+            color: 'rgb(199,86,83)'
+          },
+          fragment1: {
+            backgroundColor: '#000',
+            color: 'yellow',
+            padding: 5
+          },
+          fragment2: {
+            backgroundColor: '#339911',
+            color: '#fff',
+            borderRadius: 15,
+            padding: 5
+          }
         }
-    ],
-    xAxis: {
-        axisLabel: {show: false},
-        axisLine: {show: false},
-        splitLine: {show: false},
-        axisTick: {show: false},
-        min: -1,
-        max: 1
-    },
-    yAxis: {
-        axisLabel: {show: false},
-        axisLine: {show: false},
-        splitLine: {show: false},
-        axisTick: {show: false},
-        min: -1,
-        max: 1
+      }
     }
+  ],
+  xAxis: {
+    axisLabel: { show: false },
+    axisLine: { show: false },
+    splitLine: { show: false },
+    axisTick: { show: false },
+    min: -1,
+    max: 1
+  },
+  yAxis: {
+    axisLabel: { show: false },
+    axisLine: { show: false },
+    splitLine: { show: false },
+    axisTick: { show: false },
+    min: -1,
+    max: 1
+  }
 };
