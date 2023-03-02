2392e5701f3b9bb6c7a29d5d8473021d61d47522
Guo Yunhe <i@guoyunhe.me>
2023-03-02 13:45:33 +0800

    Update pie-half-donut.ts

--- public/examples/ts/pie-half-donut.ts
+++ public/examples/ts/pie-half-donut.ts
@@ -1,7 +1,7 @@
 /*
-title: Doughnut Chart
+title: Half Doughnut Chart
 category: pie
-titleCN: 环形图
+titleCN: 半环形图
 difficulty: 1
 */
 
@@ -11,13 +11,16 @@ option = {
   },
   legend: {
     top: '5%',
-    left: 'center'
+    left: 'center',
+    selectedMode: false, // doesn't work with our tricks, disable it
   },
   series: [
     {
       name: 'Access From',
       type: 'pie',
-      radius: ['40%', '70%'],
+      radius: ['70%', '140%'],
+      center: ['50%', '100%'], // move down the chart to hide the bottom half
+      startAngle: 180, // adjust the start angle
       avoidLabelOverlap: false,
       label: {
         show: false,
@@ -38,7 +41,14 @@ option = {
         { value: 735, name: 'Direct' },
         { value: 580, name: 'Email' },
         { value: 484, name: 'Union Ads' },
-        { value: 300, name: 'Video Ads' }
+        { value: 300, name: 'Video Ads' },
+        {
+          value: 1048 + 735 + 580 + 484 + 300, // make an record to fill the bottom 50%
+          name: '', // hide the legend
+          itemStyle: {
+            color: '#ffffff', // make the piece invisible
+          },
+        },
       ]
     }
   ]
