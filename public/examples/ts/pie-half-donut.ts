63e08d8a4165b7412e5a35fe11b2ce357042f038
Guo Yunhe <i@guoyunhe.me>
2023-03-02 13:37:12 +0800

    Create pie-half-donut.ts

---
+++ public/examples/ts/pie-half-donut.ts
@@ -0,0 +1,46 @@
+/*
+title: Doughnut Chart
+category: pie
+titleCN: 环形图
+difficulty: 1
+*/
+
+option = {
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    top: '5%',
+    left: 'center'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: ['40%', '70%'],
+      avoidLabelOverlap: false,
+      label: {
+        show: false,
+        position: 'center'
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: 40,
+          fontWeight: 'bold'
+        }
+      },
+      labelLine: {
+        show: false
+      },
+      data: [
+        { value: 1048, name: 'Search Engine' },
+        { value: 735, name: 'Direct' },
+        { value: 580, name: 'Email' },
+        { value: 484, name: 'Union Ads' },
+        { value: 300, name: 'Video Ads' }
+      ]
+    }
+  ]
+};
+export {};
