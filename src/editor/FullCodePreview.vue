df41f472ad75b7230428caa43c13672dbc777be9
Kapil Sachdev <kapilsachdev03@gmail.com>
2025-10-31 17:02:31 +0530

    upgrade: Convert FullCodePreview component to script-setup Composition API

--- src/editor/FullCodePreview.vue
+++ src/editor/FullCodePreview.vue
@@ -1,82 +1,89 @@
 <template>
-  <div class="full-code-preview" v-loading="loading"></div>
+  <div ref="full-code" class="full-code-preview" v-loading="loading"></div>
 </template>
 
-<script>
+<script setup>
+import {
+  ref,
+  onMounted,
+  onBeforeUnmount,
+  watch,
+  useTemplateRef,
+  reactive
+} from 'vue';
 import { loadScriptsAsync } from '../common/helper';
 import { store } from '../common/store';
 import { getScriptURLs } from '../common/config';
 
+const { code } = defineProps({
+  code: String
+});
+
+const shared = reactive(store);
+
+const fullCodeElement = useTemplateRef('full-code');
+
+const loading = ref(false);
+const editor = ref(null);
+
 function ensureACE() {
   if (typeof ace === 'undefined') {
-    const SCRIPT_URLS = getScriptURLs(store.locale);
+    const SCRIPT_URLS = getScriptURLs(shared.locale);
 
     return loadScriptsAsync([SCRIPT_URLS.aceDir + '/ace.js']);
   }
   return Promise.resolve();
 }
 
-export default {
-  props: ['code'],
-
-  data() {
-    return {
-      shared: store,
-      loading: false
-    };
-  },
-
-  mounted() {
-    this.loading = true;
-    ensureACE().then(() => {
-      this.loading = false;
-      const editor = ace.edit(this.$el);
-      editor
-        .getSession()
-        .setMode(
-          store.typeCheck ? 'ace/mode/typescript' : 'ace/mode/javascript'
-        );
-      // https://stackoverflow.com/questions/32806060/is-there-a-programmatic-way-to-hide-the-cursor-in-ace-editor
-      editor.setOptions({
-        readOnly: true,
-        showLineNumbers: false,
-        showFoldWidgets: false,
-        highlightActiveLine: false,
-        highlightGutterLine: false
-      });
-      // editor.renderer.setShowGutter(false);
-      editor.renderer.$cursorLayer.element.style.display = 'none';
-
-      this._editor = editor;
-
-      this.setCode(this.code);
+onMounted(() => {
+  loading.value = true;
+  ensureACE().then(() => {
+    loading.value = false;
+    const aceEditor = ace.edit(fullCodeElement.value);
+    aceEditor
+      .getSession()
+      .setMode(
+        shared.typeCheck ? 'ace/mode/typescript' : 'ace/mode/javascript'
+      );
+    // https://stackoverflow.com/questions/32806060/is-there-a-programmatic-way-to-hide-the-cursor-in-ace-editor
+    aceEditor.setOptions({
+      readOnly: true,
+      showLineNumbers: false,
+      showFoldWidgets: false,
+      highlightActiveLine: false,
+      highlightGutterLine: false
     });
-  },
+    // editor.renderer.setShowGutter(false);
+    aceEditor.renderer.$cursorLayer.element.style.display = 'none';
 
-  methods: {
-    setCode(code) {
-      if (this._editor) {
-        this._editor.setValue(code);
-        this._editor.selection.setSelectionRange({
-          start: {
-            row: 1,
-            column: 4
-          },
-          end: {
-            row: 1,
-            column: 4
-          }
-        });
+    editor.value = aceEditor;
+
+    setCode(code);
+  });
+});
+
+function setCode(code) {
+  if (editor.value) {
+    editor.value.setValue(code);
+    editor.value.selection.setSelectionRange({
+      start: {
+        row: 1,
+        column: 4
+      },
+      end: {
+        row: 1,
+        column: 4
       }
-    }
-  },
-
-  watch: {
-    code(newVal) {
-      this.setCode(newVal);
-    }
+    });
   }
-};
+}
+
+watch(
+  () => code,
+  (newVal) => {
+    setCode(newVal);
+  }
+);
 </script>
 
 <style lang="scss">
