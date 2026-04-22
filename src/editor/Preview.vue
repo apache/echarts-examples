903c6905c7c90af014eef3adcc10daf6898d54d3
Justin-ZS <jzstxdy@gmail.com>
2026-04-22 09:28:05 +0800

    fix(editor): restore PR preview bundle routing

--- src/editor/Preview.vue
+++ src/editor/Preview.vue
@@ -186,13 +186,11 @@ function getScripts(nightly) {
         : 'echartsDir'
     ];
 
-  // TODO CSP 问题
-  // const echartsDir = store.isPR
-  //   ? echartsDirTpl.replace('{{PR_NUMBER}}', store.prNumber)
-  //   : echartsDirTpl.replace('{{version}}', store.echartsVersion);
   const echartsDir = isLocal
     ? SCRIPT_URLS.localEChartsDir
-    : SCRIPT_URLS.latestEChartsDir;
+    : store.isPR
+    ? echartsDirTpl.replace('{{PR_NUMBER}}', store.prNumber)
+    : echartsDirTpl.replace('{{version}}', store.echartsVersion);
 
   const code = store.runCode;
 
