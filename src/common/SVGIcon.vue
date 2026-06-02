6928fc81baa7d34aacec67d5764058ce3c99b4ce
Kapil Sachdev <kapilsachdev03@gmail.com>
2026-06-02 17:42:31 +0530

    feat(ui): Add SVGIcon component and replace inline SVGs

---
+++ src/common/SVGIcon.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg
+    role="img"
+    class="svg-icon"
+    :class="className"
+    :width="size"
+    :height="size"
+    v-bind="$attrs"
+  >
+    <use :href="symbolHref"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'SVGIcon',
+  props: {
+    name: { type: String, required: true },
+    size: { type: [Number, String], default: 12 },
+    className: { type: [String, Array, Object], default: '' }
+  },
+  computed: {
+    symbolHref() {
+      return `../asset/sprite.svg#${this.name}`;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.svg-icon {
+  display: inline-block;
+  vertical-align: middle;
+}
+</style>
