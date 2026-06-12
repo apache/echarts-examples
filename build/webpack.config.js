77f6b17631afd17b59f5c26833dde3c201593098
Kapil Sachdev <kapilsachdev03@gmail.com>
2026-06-12 18:37:22 +0530

    chore: Update babel and its webpack loader

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -47,11 +47,17 @@ module.exports = (env, argv) => {
           },
           {
             test: /\.js$/,
+            exclude: /node_modules/,
             resourceQuery: {
               not: [/raw-pure/]
             },
-            use: ['babel-loader'],
-            exclude: /node_modules/
+            use: {
+              loader: 'babel-loader',
+              options: {
+                targets: 'defaults',
+                presets: [['@babel/preset-env']]
+              }
+            }
           },
           {
             test: /\.css$/,
